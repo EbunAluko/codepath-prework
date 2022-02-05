84a96a3d05fe1a1d4766b099a364920c79b8fce8
EbunAluko <98724992+EbunAluko@users.noreply.github.com>
2022-02-04 20:16:44 -0500

    Added pre-work files to repository

---
+++ EbunPrework/EbunPrework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EbunPrework
+//
+//  Created by Ebun Aluko on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+        self.view.backgroundColor = UIColor.black
+    }
+    
+}
+
